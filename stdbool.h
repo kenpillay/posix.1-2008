79aec8ef00e0a610e4b75cd8e11343040dc3efd2
Ken Pillay <ken@pillay.com>
2012-06-16 13:51:41 -0700

    add stdbool.h

---
+++ stdbool.h
@@ -0,0 +1,15 @@
+/* 
+filename:		stdbool.h
+specification:	The Open Group, 2008
+license:		GPLv2
+implementation:	yallip(R) 2012
+note:			The Open Group defines these constants of type integer.
+				They are herein defined as minimum bit representations.
+*/
+
+#define	bool	_Bool
+
+#define	true	0x01h
+#define	false	0x00h
+
+#define	__bool_true_false_are_defined	0x01h
